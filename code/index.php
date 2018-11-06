ddb6e46b06279b7c5121586ea2ee1076dbb6a11c
DominatorProgramator <st49522@student.upce.cz>
2018-11-06 18:59:11 +0100

    Adding login form, register form and config db

--- code/index.php
+++ code/index.php
@@ -1,4 +1,9 @@
-<?php
-echo "Player";
-?>
+<html>
+<head>
+    <title>Welcome to Playbox</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
 
+</body>
+</html>
--- code/register.php
+++ code/register.php
@@ -1,3 +1,42 @@
-<?php
-echo "Registrace";
-?>
+<html>
+<head>
+    <title>Playbox | Login page</title>
+    <link rel="stylesheet" type="text/css" href="assets/css/register.css">
+</head>
+<body>
+
+<form id="loginForm" action="register.php" method="POST">
+    <h3>Login to your account</h3>
+    <p>
+        <input type="text" id="loginUsername" name="loginUsername" placeholder="Username" class="inputForm" required>
+    </p>
+    <p>
+        <input type="password" id="loginPasswd" name="loginPasswd" placeholder="Password" class="inputForm" required>
+    </p>
+    <button class="btn" type="submit" name="loginBtn">Log in</button>
+</form>
+
+<form id="registerForm" action="register.php" method="POST">
+    <h3>Create your free account</h3>
+    <p>
+        <input type="text" id="username" name="username" placeholder="Username" class="inputForm" required>
+    </p>
+    <p>
+        <input type="text" id="firstName" name="firstName" placeholder="First name" class="inputForm" required>
+    </p>
+    <p>
+        <input type="text" id="lastName" name="lastName" placeholder="Last name" class="inputForm" required>
+    </p>
+    <p>
+    <input type="email" id="email" name="email" placeholder="Email" class="inputForm" required>
+    </p>
+    <p>
+        <input type="password" id="passwd" name="passwd" placeholder="Password" class="inputForm" required>
+    </p>
+    <p>
+        <input type="password" id="passwd2" name="passwd2" placeholder="Confirm password" class="inputForm" required>
+    </p>
+    <button class="btn" type="submit" name="registerBtn">Sign up</button>
+</form>
+</body>
+</html>
